4799c57b5a970f497b5e00a3968a1441b268aba7
sridhardurgam <sridhar.durgam@symphonysv.com>
2012-07-06 16:33:32 +0530

    Discarded remote changes.

--- NalamProjectForSridhar Repositr/src/com/symphony/NHello.java
+++ NalamProjectForSridhar Repositr/src/com/symphony/NHello.java
@@ -14,8 +14,6 @@ public class NHello {
 	System.out.println("Hi Sridhar adding the third line ");
 	System.out.println("hi how r u");
 	System.out.println("hi 5th line added");
-	System.out.println("hi 6th line added");
-	System.out.println("hi 7th line added");
-	
+		
 }
 }
