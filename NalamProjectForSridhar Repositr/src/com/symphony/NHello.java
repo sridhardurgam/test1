feda0a75bf4e00bfaa37cca7125dabd4097baf52
qamroon <alam.btech@gmail.com>
2012-07-06 16:21:51 +0530

    discarded locla changes.

--- NalamProjectForSridhar Repositr/src/com/symphony/NHello.java
+++ NalamProjectForSridhar Repositr/src/com/symphony/NHello.java
@@ -13,8 +13,8 @@ public class NHello {
 	System.out.println("Hi Sridhar adding the second line ");
 	System.out.println("Hi Sridhar adding the third line ");
 	System.out.println("hi how r u");
-	System.out.println("hi 5th line added");
-	System.out.println("hi 6th line added");
+	
+	
 	
 }
 }
